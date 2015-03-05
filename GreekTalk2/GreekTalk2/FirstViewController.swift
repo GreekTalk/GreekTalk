81c6cdc742924359e49216bf87a75e92521dfa1d
raulgrogovich <crayjaeger@Crays-MacBook-Pro.local>
2015-03-04 23:19:31 -0600

    Created Xcode Project

---
+++ GreekTalk2/GreekTalk2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GreekTalk2
+//
+//  Created by Cray Jaeger on 3/4/15.
+//  Copyright (c) 2015 Cray Jaeger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GreekTalk2/GreekTalk2Tests/GreekTalk2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GreekTalk2Tests.swift
+//  GreekTalk2Tests
+//
+//  Created by Cray Jaeger on 3/4/15.
+//  Copyright (c) 2015 Cray Jaeger. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GreekTalk2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
